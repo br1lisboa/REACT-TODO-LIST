bdd080085a6ab1307c0b15c9d008b62568c5d69f
br1lisboa <brlisbo@gmail.com>
2022-07-14 09:34:48 -0300

    trabajanden funcionalidad de agregar tareas, eliminarlas, degrade

--- src/components/ListaDeTareas.jsx
+++ src/components/ListaDeTareas.jsx
@@ -6,16 +6,35 @@ import Tarea from './Tarea';
 
 function ListaDeTareas() {
 	const [tareas, setTareas] = useState([]);
+	
+	const agregarTarea = tarea => {
+		if (tarea.texto.trim()) {
+			tarea.texto = tarea.texto.trim(); /* trim() es un metodo que nos permite sacar los espacios del principio o del final de una cadena de caracteres */
+			const tareasActualizadas = [tarea, ...tareas];
+			setTareas(tareasActualizadas);
+		}
+	}
+
+	const eliminarTarea = id => {
+		const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+		/* Aca, tareasActualizadas, va a ser el arreglo filtrado, con el metodo filter, luego filtramos
+		mediante el que cumpla esta condicion va a ser incluido en el arreglo, pero si el id es igual 
+		al que estamos pidiendo no lo vamos a incluir, generando una copia nueva. */
+		setTareas(tareasActualizadas);
+	}
 
 	return (
 		<>
-			<TareaFormulario />
+			<TareaFormulario onSubmit={agregarTarea} />
 			<div className='tareas-lista-contenedor'>
 				{
 					tareas.map((tarea) => 
-						<Tarea 
+						<Tarea
+							key={tarea.id} /* key es una forma que tiene react de identificar nuestro elemento en una lista */ 
+							id={tarea.id}
 							texto={tarea.texto}
 							completada={tarea.completada}
+							eliminarTarea={eliminarTarea}
 						/>
 					)
 				}
--- src/components/TareaFormulario.jsx
+++ src/components/TareaFormulario.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 import './styles/TareaFormulario.css';
+import { v4 as uuidv4} from 'uuid';
 
 const TareaFormulario = (props) => {
+	const [input, setInput] = useState('');
+	
+	const manejarCambio = e => {
+		setInput(e.target.value);
+		/* console.log(e.target.value); */
+	}
+
+	const manejarEnvio = e => {
+		e.preventDefault();
+		const tareaNueva = {
+			id: uuidv4(), /* paquete uuid para generar identificadores unicos */
+			texto: input,
+			completada: false
+		}
+		props.onSubmit(tareaNueva); /* esta funcion va a ser un props, que pasaremos a la lista de tareas */
+	}						
+
 	return(
-		<form className='tarea-formulario'>
+		<form 
+		className='tarea-formulario'
+		onSubmit={manejarEnvio}> {/* Evento escuchador */}
 			
 			<input type="text"
 			className='tarea-input'
 			placeholder='Escribe una Tarea'
-			name='texto' 
+			name='texto'
+			onChange={manejarCambio} 
 			/>
 			
 			<button className='tarea-boton'>
